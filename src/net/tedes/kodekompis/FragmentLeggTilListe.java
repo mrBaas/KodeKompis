8bfec420bfe828b0cd99473a11cfc5dd25655d4d
Martin <martin.alexander.nygren@gmail.com>
2014-04-01 17:05:43 +0200

    Dialogfixing.

--- src/net/tedes/kodekompis/FragmentLeggTilListe.java
+++ src/net/tedes/kodekompis/FragmentLeggTilListe.java
@@ -55,9 +55,13 @@ public class FragmentLeggTilListe extends DialogFragment implements OnClickListe
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.fragment_dialog_create, container);
         
-        getDialog().getWindow().requestFeature(Window.FEATURE_NO_TITLE); 
-        getDialog().getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);
-        getDialog().getWindow().setBackgroundDrawable(new ColorDrawable(Color.TRANSPARENT));
+        WindowManager.LayoutParams lp = new WindowManager.LayoutParams();
+        Window window = getDialog().getWindow();
+        window.requestFeature(Window.FEATURE_NO_TITLE); 
+        window.setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);
+        window.setBackgroundDrawable(new ColorDrawable(Color.TRANSPARENT));
+        
+      
         
         
         mAvbryt = (Button)view.findViewById(R.id.dialog_button_avbryt);
